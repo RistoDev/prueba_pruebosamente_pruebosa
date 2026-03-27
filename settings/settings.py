2b0ff155f476e3a4ff997322a8f32795d7e6eb30
RistoDev <97709114+RistoDev@users.noreply.github.com>
2026-03-26 21:10:22 -0600

    a

---
+++ settings/settings.py
@@ -0,0 +1,7 @@
+CAMERA_INDEX = 2
+CONFIDENCE_THRESHOLD = 0.5
+CLASSES_NAME = ['Metal', 'Vidrio', 'Plastico', 'Carton', 'Medico']
+DATASET_DIR_CUT = 'dataset/cut'
+DATASET_DIR_FULL = 'dataset/full'
+SECONDS_TO_SAVE_CUT = 5
+SECONDS_TO_SAVE_FULL = 5
